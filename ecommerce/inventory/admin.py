d69f89b2d1ae9a8b7c76d82571e6e8d88d8b207d
TristanLilford <tristanlilford@gmail.com>
2024-10-29 19:30:21 +0200

    add update and delete

--- ecommerce/inventory/admin.py
+++ ecommerce/inventory/admin.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductCategories
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'popularity_score')
     search_fields = ('name', 'category')
+
+@admin.register(ProductCategories)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- ecommerce/inventory/views.py
+++ ecommerce/inventory/views.py
@@ -4,15 +4,19 @@ from django.views.generic import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from core.mixins import AdminRequiredMixin
+from .forms import ProductForm
 
 from .models import Product
 
 
 class ProductListView(LoginRequiredMixin, ListView):
     model = Product
-    template_name = 'inventory/product_list.html'  # Specify the template
+    template_name = 'inventory/products.html'  # Specify the template
     context_object_name = 'products'  # Name for the list in the template
     paginate_by = 10  # Optional: add pagination if there are many products
+
+    def get_queryset(self):
+        return Product.objects.prefetch_related('categories').all()
 
 
 class ProductDetailView(DetailView):
@@ -24,7 +28,7 @@ class ProductDetailView(DetailView):
 class ProductCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
     model = Product
     template_name = 'inventory/product_form.html'
-    fields = ['product_id', 'name', 'category', 'price', 'quantity', 'popularity_score']
+    form_class = ProductForm
     success_url = reverse_lazy('inventory:products')
 
     def form_valid(self, form):
@@ -34,14 +38,16 @@ class ProductCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
 
 class ProductEditView(UpdateView):
     model = Product
+    form_class = ProductForm
     template_name = "inventory/product_edit.html"
 
     def get_success_url(self):
-        return reverse_lazy('inventory:product_list')
+        return reverse_lazy('inventory:products')
+
 
 class ProductDeleteView(DeleteView):
     model = Product
     template_name = "inventory/product_confirm_delete.html"
 
     def get_success_url(self):
-        return reverse_lazy('inventory:product_list')
+        return reverse_lazy('inventory:products')
