131f8698efab7c7305629aa018b02237dc62bcee
TristanLilford <tristanlilford@gmail.com>
2024-10-30 22:07:46 +0200

    edit compose and settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('/app/db', 'db.sqlite3'),  # Store db.sqlite3 in the /app/db directory
     }
 }
 
