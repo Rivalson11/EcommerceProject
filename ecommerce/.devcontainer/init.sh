2eacc36273f4278a565b20b83c9ff25c52fb2fb8
TristanLilford <tristanlilford@gmail.com>
2024-10-30 22:39:06 +0200

    change to run fixtures on start up

--- ecommerce/.devcontainer/init.sh
+++ ecommerce/.devcontainer/init.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-
 # Run migrations
 python manage.py migrate
+
+# Load fixture data
+python manage.py loaddata your_app/fixtures/initial_data.json
 
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Start Celery worker and beat services
+# Start Celery and Django
 celery -A ecommerce worker --loglevel=info &
 celery -A ecommerce beat --loglevel=info &
+python manage.py runserver 0.0.0.0:8000
