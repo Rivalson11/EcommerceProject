2f8cdbf191f5e02d6bb8c64a83ddb3299cb9f671
TristanLilford <tristanlilford@gmail.com>
2024-10-30 22:26:59 +0200

    add root url redirect

--- ecommerce/users/urls.py
+++ ecommerce/users/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('', views.redirect_to_login, name='root_redirect'),
     path("accounts/login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("register/", views.RegisterView.as_view(), name="register"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
