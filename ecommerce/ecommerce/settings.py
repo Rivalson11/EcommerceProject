ba02f49728802c9156b1c45b11106478bbfb41a0
TristanLilford <tristanlilford@gmail.com>
2024-10-31 00:41:00 +0200

    change to use diff file location for storaghe

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -15,8 +15,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -130,13 +129,14 @@ CELERY_RESULT_BACKEND = "django-db"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # BASE_DIR should be the root project directory
-]
 
 # settings.py
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 MEDIA_URL = "/media/"
 
 
